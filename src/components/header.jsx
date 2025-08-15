915ed4e0f5b6977f99cfe49de1ad9974fe203dd5
Mathilde <mathilde.bruzat@gmail.com>
2025-08-15 18:55:52 +0200

    jfzdi'

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import '../main.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../actions/user.action';
-import { argentBankLogo } from '../assets/argentBankLogo.webp'
+import argentBankLogo from "../assets/argentBankLogo.webp"
 
 
 function Header () {
@@ -24,8 +24,7 @@ function Header () {
       <Link className="main-nav-logo" to="/">
         <img
           className="main-nav-logo-image"
-          src={argentBankLogo.webp}  
-          alt="Argent Bank Logo"
+src={argentBankLogo}          alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
