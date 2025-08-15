31e363ee2f2fb1de248a31903e03b52d1961260f
Mathilde <mathilde.bruzat@gmail.com>
2025-08-15 18:27:09 +0200

    changement src img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@
       <section className="features">
         <h2 className="sr-only">Features</h2>
         <div className="feature-item">
-          <img src="../public/icon-chat.webp" alt="Chat Icon" className="feature-icon" />
+          <img src="/icon-chat.webp" alt="Chat Icon" className="feature-icon" />
           <h3 className="feature-item-title">You are our #1 priority</h3>
           <p>
             Need to talk to a representative? You can get in touch through our
@@ -17,7 +17,7 @@
         </div>
         <div className="feature-item">
           <img
-            src="../public/icon-money.webp"
+            src="/icon-money.webp"
             alt="Money Icon"
             className="feature-icon"
           />
@@ -28,7 +28,7 @@
         </div>
         <div className="feature-item">
           <img
-            src="../public/icon-security.webp"
+            src="/icon-security.webp"
             alt="Security Icon"
             className="feature-icon"
           />
